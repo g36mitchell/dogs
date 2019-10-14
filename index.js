50c82d1c1fc28af9ddf8cacfac9fe816d9b1f090
Gail Mitchell <g36mitchell@gamil.com>
2019-10-14 01:00:57 -0500

    functionally working

--- index.js
+++ index.js
@@ -1,134 +1,155 @@
 'use strict';
 
 function generateSliderElement(item) {
-      return `<li class="slide"><img src="${item}" /></li>`;
-  }
+  return `<li class="slides"><img src="${item}" /></li>`;
+}
+
+function generateSliderItemsString(pictureList) {
+
+  const items = pictureList.map((item) => generateSliderElement(item)); 
+  
+   return items.join("");
+ }
+
+
 
 function generateDropdownElement(item) {
-      return `<li>${item}</li>`;
-}
-  
-function generateSliderItemsString(pictureList) {
-  
-      const items = pictureList.map((item) => generateSliderElement(item));
-      return items.join("");
+
+  // Capitalize the first letter of each word
+  let itemCapitalize = item.display.split(" ");
+  for (var i = 0, x = itemCapitalize.length; i < x; i++) {
+    itemCapitalize[i] = itemCapitalize[i][0].toUpperCase() + itemCapitalize[i].substr(1);
+  }
+  item.display = itemCapitalize.join(" ");
+
+  // format the dropdown item
+  return `<option value="${item.api}">${item.display}</option>`;
+
 }
 
 function generateDropdownString(breedList) {
-      const items = breedList.map((item) => generateDropdownElement(item));
-      return items.join("");
-}
   
+    let items = [];
+
+/*  const items = breedList.map((item) => generateDropdownElement(item)); */
+
+    for (let i = 0; i < breedList.length; i++) {        
+        items.push(generateDropdownElement(breedList[i]));
+    } 
+
+    return items.join("");
+}
+
 function getDogImages(quantity) {
 
   fetch(`https://dog.ceo/api/breeds/image/random/${quantity}`)
     .then(response => response.json())
-    .then(responseJson => { 
-            let dogSliderItems =  generateSliderItemsString(responseJson.message);
-            displayDogImages(dogSliderItems);
+    .then(responseJson => {
+      let dogSliderItems = generateSliderItemsString(responseJson.message);
+      displayDogImages(dogSliderItems);
     })
     .catch(error => alert('Oops! Something went wrong. Try again later.'));
 }
 
-function flatten(obj, list) {
-  for (key in obj) {
-    if (obj[key].length == 0) {
-        console.log(`${key}`);
-        list.push(key);
-    }
-    else {
-       for (i = 0; i < (obj[key].length); i++) {
-            console.log(`${obj[key][i]} ${key}`);
-            list.push(`${obj[key][i]} ${key}`);
-       } 
-    }
-  }
+function getDogImage(breed) {
 
-  return true;
+  fetch(`https://dog.ceo/api/breed/${breed}/images/random`)
+    .then(response => response.json())
+    .then(responseJson => {
+      let dogSliderItems = 
+       `<li class="slides"><img src="${responseJson.message}" /></li>`;
+      displayDogImage(dogSliderItems);
+    })
+    .catch(error => alert('Oops! Something went wrong. Try again later.'));
 }
+
 
 function formatListOfBreeds() {
 
   fetch("https://dog.ceo/api/breeds/list/all")
-  .then(response => response.json())
-  .then(responseJson => {
-            let flattenedList = [];
-//            let flattenedBreed = "";
-            let obj = responseJson.message;  /* interested only in the list of breeds */
-            let listOfKeys = Object.keys(obj);
-            console.log(`${Object.keys(obj)}`);
-            for (let j = 0; j < listOfKeys.length; j++) {
-                console.log(`${listOfKeys[j]}`);
-                if (obj[listOfKeys[j]].length == 0) {
-                    flattenedList.push(listOfKeys[j]);
-                }
-                else {
-                    for (let i = 0; i < obj[listOfKeys[j]].length; i++) {
-                          console.log(`${obj[listOfKeys[j]][i]} ${listOfKeys[j]}`);
-                          // looks perfect in the console log but fails to push
-                          flattenendList.push((obj[listOfKeys[j]][i] + " " + listOfKeys[j]));
-                    }
-                }
-            }
-            console.log(`${flattenedList}`);
-   /*         let dogBreedItems = generateDropdownString(flattenedList);
-            prepareDogBreedList(dogBreedItems);*/
-  })
-  .catch(error => alert('UGhhhhhhh.'));
+    .then(response => response.json())
+    .then(responseJson => {
+      let flattenedList = [];
+      let obj = responseJson.message;  /* interested only in the list of breeds */
+      let listOfKeys = Object.keys(obj);
+      for (let j = 0; j < listOfKeys.length; j++) {
+        if (obj[listOfKeys[j]].length == 0) {
+          flattenedList.push({"display": listOfKeys[j], 
+                              "api": listOfKeys[j]
+                            });
+        }
+        else {
+          for (let i = 0; i < obj[listOfKeys[j]].length; i++) {
+            flattenedList.push({"display": obj[listOfKeys[j]][i] + " " + listOfKeys[j], 
+                                "api": listOfKeys[j] + "/" + obj[listOfKeys[j]][i]
+                              });
+          }
+        }
+      }
+      console.log(flattenedList);
+      let optionList = generateDropdownString(flattenedList);
+      $('#js-breed-list').html(optionList);
+    })
+    .catch(error => alert('Oops! Something went wrong. Try again later.'));
 }
 
 function displayDogImages(pictureListString) {
 
-    $('.js-slides').html(pictureListString);
-		$('.js-display-dog-pictures').show();
+  $('#js-slides-any-breed').html(pictureListString);
+  $('#js-slider-any-breed').show();
 }
 
-function displayDogBreeds(dogBreedListString) {
-  $('#js-breed-list').html(dogBreedListString);
+function displayDogImage(pictureListString) {
+
+  $('#js-slides-specific-breed').html(pictureListString);
+  $('#js-slider-specific-breed').show();
 }
 
 function watchFormAnyBreed() {
-  $('.js-form-any-breed').submit(event => {
-    event.preventDefault();
 
+  $('#js-form-any-breed').submit(event => {
+    event.preventDefault();
     let quantity = $('.js-number-of-dogs').val();
     getDogImages(quantity);
-
   });
 }
 
 
 function watchFormSpecificBreed() {
-      console.log("inside watchFormSpecificBreeds.");
+  console.log("inside watchFormSpecificBreeds.");
+  $('#js-form-specific-breed').submit(event => {
+    event.preventDefault();
+
+    let breed = $('#js-breed-list').val();
+    getDogImage(breed);
+
+  });
 }
 
 function watchForm() {
 
-  $('.js-pick-options').on('click', 'input[type=radio]', function(event) {
+  $('#js-pick-options').on('click', 'input[type=radio]', function (event) {
 
-    let optionSelected = $('.js-pick-options input:checked').attr('value');
-    
+    let optionSelected = $('#js-pick-options input:checked').attr('value');
+
     console.log(optionSelected);
 
-    switch(optionSelected) {
+    switch (optionSelected) {
       case "anyBreed":
-            $('.js-any-breed').show();
-            $('.js-specific-breed').hide();
-            watchFormAnyBreed();
+        $('#js-any-breed').show();
+        $('#js-specific-breed').hide();
+        watchFormAnyBreed();
         break;
       case "specificBreed":
-            $('.js-any-breed').hide();
-            $('.js-specific-breed').show();
-/*            watchFormSpecificBreed(); */
+        $('#js-any-breed').hide();
+        $('#js-specific-breed').show();
+        watchFormSpecificBreed();
         break;
     }
-
-
   });
-  
 }
 
-$(function() {
+$(function () {
   formatListOfBreeds();
   watchForm();
 });
